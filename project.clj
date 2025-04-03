52f6071819d88a46afa7ee3ca1845b4a66f6ea6c
Ankayarkanni <anakayarkanni@formcept.com>
2025-04-03 17:08:44 +0530

    Update to support JDK-17 and Scala-2.12

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject yieldbot/serializable-fn "0.1.2"
+(defproject yieldbot/serializable-fn "0.1.3"
   :url "http://github.com/sorenmacbeth/serializable-fn"
   :description "Serializable functions in Clojure"
   :min-lein-version "2.0.0"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :javac-options ["-source" "1.6" "-target" "1.6"]
   :jvm-opts ^:replace ["-server" "-Xmx512m"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [com.twitter/chill-java "0.8.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [com.esotericsoftware/kryo "5.6.1"]
+                 [com.twitter/chill-java "0.10.0"]
                  [com.twitter/carbonite "1.5.0"]]
   :profiles {:dev
-             {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]
-                             [criterium "0.4.3"]]}
+             {:dependencies [[ch.qos.logback/logback-classic "1.5.18"]
+                             [criterium "0.4.6"]]}
              :provided
-             {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]]}})
+             {:dependencies [[ch.qos.logback/logback-classic "1.5.18"]]}})
